4ea5a59e655659c7bbe97b69ce658de07c9550ff
sirh3e <sirh3e@protonmail.com>
2020-09-15 19:39:09 +0200

    refactored func INC

--- main.c
+++ main.c
@@ -201,9 +201,7 @@ int instruction_inc(Vm* vm){
     assert(vm != NULL);
     assert(vm->instructions[vm->instruction_index] < INSTRUCTION_LENGTH); //a tmp variable would be cheating
 
-    vm->registers[vm->instructions[vm->instruction_index]] = vm->registers[vm->instructions[vm->instruction_index]] + vm->instructions[vm->instruction_index + 1];
-
-    vm->instruction_index++;
+    vm->registers[vm->instructions[vm->instruction_index]] += vm->instructions[vm->instruction_index++];
     vm->instruction_index++;
 
     return 0;
