89dc3dd385abb6e165200226db5b87265a91ef0f
sirh3e <sirh3e@protonmail.com>
2020-09-20 23:31:11 +0200

    refactored src/instruction/shr.c

---
+++ src/instruction/shr.c
@@ -0,0 +1,18 @@
+//
+// Created by sirh3e on 9/20/2020.
+//
+
+#include <assert.h>
+
+#include "../instructions.h"
+
+int instruction_shr(Vm* vm){ //right
+
+    assert(vm != NULL);
+    assert(vm->instructions[vm->instruction_index] < INSTRUCTION_LENGTH);
+
+    vm->registers[vm->instructions[vm->instruction_index]] >>= vm->instructions[vm->instruction_index++];
+    vm->instruction_index++;
+
+    return 0;
+}
