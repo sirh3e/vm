08a95bf1a25b3e217411aa5d39e792c36ee50157
sirh3e <sirh3e@protonmail.com>
2020-09-20 19:03:28 +0200

    refactored instruction add

--- src/instruction/add.c
+++ src/instruction/add.c
@@ -2,4 +2,22 @@
 // Created by sirh3e on 9/20/2020.
 //
 
+#include <assert.h>
+
+#include "../registers.h"
+#include "../macros.h"
+
 #include "add.h"
+
+int instruction_add(Vm* vm){
+
+    assert(vm != NULL);
+
+    vm->registers[A] = VM_INSTRUCTION_GET(vm); vm->instruction_index++;
+    vm->registers[B] = VM_INSTRUCTION_GET(vm); vm->instruction_index++;
+
+    vm->registers[C] = vm->registers[A] + vm->registers[B];
+    vm->stack[vm->stack_index++] = vm->registers[C];
+
+    return 0;
+}
--- src/instruction/add.h
+++ src/instruction/add.h
@@ -2,7 +2,13 @@
 // Created by sirh3e on 9/20/2020.
 //
 
-#ifndef VM_ADD_H
-#define VM_ADD_H
+#ifndef VM_INSTRUCTION_ADD_H
+#define VM_INSTRUCTION_ADD_H
 
-#endif //VM_ADD_H
+#ifndef VM_VM_H
+    #include "../vm.h"
+#endif
+
+int instruction_add(Vm*);
+
+#endif //VM_INSTRUCTION_ADD_H
