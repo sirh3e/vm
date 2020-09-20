28d8b57b1ee3679b4514d82ed827286cfae76fb6
sirh3e <sirh3e@protonmail.com>
2020-09-20 23:24:01 +0200

    refactored instruction/mul.c

---
+++ src/instruction/mul.c
@@ -0,0 +1,22 @@
+//
+// Created by sirh3e on 9/20/2020.
+//
+
+#include <assert.h>
+
+#include "../instructions.h"
+#include "../registers.h"
+#include "../macros.h"
+
+int instruction_mul(Vm* vm){
+
+    assert(vm != NULL);
+
+    vm->registers[A] = VM_INSTRUCTION_GET(vm); vm->instruction_index++;
+    vm->registers[B] = VM_INSTRUCTION_GET(vm); vm->instruction_index++;
+
+    vm->registers[C] = vm->registers[A] * vm->registers[B];
+    vm->stack[vm->stack_index++] = vm->registers[C];
+
+    return 0;
+}
