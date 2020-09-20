ffd91ad6ccf1fdf91b0ce3048eb98b37f9ea6345
sirh3e <sirh3e@protonmail.com>
2020-09-20 23:30:14 +0200

    refactored instruction/xor.c

---
+++ src/instruction/xor.c
@@ -0,0 +1,22 @@
+//
+// Created by sirh3e on 9/20/2020.
+//
+
+#include <assert.h>
+
+#include "../instructions.h"
+#include "../registers.h"
+#include "../macros.h"
+
+int instruction_xor(Vm* vm){
+
+    assert(vm != NULL);
+    assert(vm->instructions[vm->instruction_index] < INSTRUCTION_LENGTH);
+
+    vm->registers[A] = VM_INSTRUCTION_GET(vm); vm->instruction_index++;
+    vm->registers[B] = VM_INSTRUCTION_GET(vm); vm->instruction_index++;
+
+    vm->registers[C] = vm->registers[A] ^ vm->registers[B];
+
+    return 0;
+}
