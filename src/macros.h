6ecdfc300e470a2c4004443bf8c81fc3fe6f6110
sirh3e <sirh3e@protonmail.com>
2020-09-27 14:57:20 +0200

    added VM_REGISTER_ASSERT into marcos.h

--- src/macros.h
+++ src/macros.h
@@ -13,6 +13,8 @@
 
 #define VM_INSTRUCTION_INDEX_ASSERT(vm, amount) assert(vm->instruction_index + amount <= vm->instruction_length)
 
+#define VM_REGISTER_ASSERT(register_index) assert(register_index < REGISTER_LENGTH)
+
 #define STACK_DEFAULT_ALLOCATED_SIZE 4096
 #define INSTRUCTION_DEFAULT_ALLOCATED_SIZE 4096
 
