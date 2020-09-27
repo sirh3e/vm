bc6916060a15bcdc99ff5b01038544b2497ec8c5
sirh3e <sirh3e@protonmail.com>
2020-09-27 14:11:18 +0200

    refactored src/instruction/pop/c

--- src/instruction/pop.c
+++ src/instruction/pop.c
@@ -5,12 +5,15 @@
 #include <assert.h>
 
 #include "../instructions.h"
+#include "../macros.h"
 
 int instruction_pop(Vm* vm){
 
     assert(vm != NULL);
+    VM_STACK_INDEX_ASSERT(vm, -1);
+    VM_INSTRUCTION_INDEX_ASSERT(vm, 1);
 
-    vm->stack_index--;
+    vm->registers[vm->instructions[vm->instruction_index++]] = vm->stack[vm->stack_index--];
 
     return 0;
 }
