282d9bb43975128e86aa599528bba9277403db81
sirh3e <sirh3e@protonmail.com>
2020-09-27 15:04:33 +0200

    refactored macros.h

--- src/macros.h
+++ src/macros.h
@@ -13,7 +13,7 @@
 
 #define VM_INSTRUCTION_INDEX_ASSERT(vm, amount) assert(vm->instruction_index + amount <= vm->instruction_length)
 
-#define VM_REGISTER_ASSERT(register_index) assert(register_index < REGISTER_LENGTH)
+#define VM_REGISTER_ASSERT(register_index) assert(register_index < REGISTER_LENGTH && register_index >= 0)
 
 #define STACK_DEFAULT_ALLOCATED_SIZE 4096
 #define INSTRUCTION_DEFAULT_ALLOCATED_SIZE 4096
