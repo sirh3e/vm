3c5ff7c3497a31cec060302aed31102ebad31e6c
sirh3e <sirh3e@protonmail.com>
2020-10-05 18:34:55 +0200

    refactored test_load.c added VM_INIT, TEST_INSTRUCTION_EXECUTE_NEXT

--- test/instruction/test_load.c
+++ test/instruction/test_load.c
@@ -9,18 +9,7 @@
 #include "../../src/registers.h"
 #include "../../src/macros.h"
 
-int test_instruction_load_0();
-
-int test_instruction_load(){
-
-    i32 test_results = 0;
-
-    test_results |= test_instruction_load_0();
-
-    return test_results;
-}
-
-int test_instruction_load_0() {
+test_result_t test_instruction_load() {
 
     i32 test_results = 0;
     i32 program[] = {
@@ -43,41 +32,37 @@ int test_instruction_load_0() {
             INSTRUCTION_LOAD, C,
     };
 
-    Vm *vm = vm_new();
-
-    vm_init(vm);
-    vm_program_set(vm, program, ARRAY_LENGTH(program));
-
+    VM_INIT(program);
     TEST_BEGIN();
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->stack[vm->stack_index - 1] == 16, "vm->stack[vm->stack_index - 1] == 16");
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[A] == 16, "vm.registers[A] == 0016");
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->stack[vm->stack_index - 1] == 64, "vm->stack[vm->stack_index - 1] == 64");
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[A] == 64, "vm.registers[A] == 0064");
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->stack[vm->stack_index - 1] == 128, "vm->stack[vm->stack_index - 1] == 0128");
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[B] == 128, "vm.registers[B] == 0128");
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->stack[vm->stack_index - 1] == 256, "vm->stack[vm->stack_index - 1] == 0256");
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[B] == 256, "vm.registers[B] == 0256");
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->stack[vm->stack_index - 1] == 512, "vm->stack[vm->stack_index - 1] == 0512");
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[C] == 512, "vm.registers[C] == 0512");
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->stack[vm->stack_index - 1] == 1024, "vm->stack[vm->stack_index - 1] == 1024");
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[C] == 1024, "vm.registers[C] == 1024");
 
     TEST_END(vm);
