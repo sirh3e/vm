4559c5647b435fe5fa0bfd16316db7a6d1d5916b
sirh3e <sirh3e@protonmail.com>
2020-10-05 18:39:37 +0200

    refactored test_or.c added VM_INIT, TEST_INSTRUCTION_EXECUTE_NEXT

--- test/instruction/test_or.c
+++ test/instruction/test_or.c
@@ -9,18 +9,7 @@
 #include "../../src/registers.h"
 #include "../../src/macros.h"
 
-int test_instruction_or_0();
-
-int test_instruction_or(){
-
-    i32 test_results = 0;
-
-    test_results |= test_instruction_or_0();
-
-    return test_results;
-}
-
-int test_instruction_or_0(){
+test_result_t test_instruction_or(){
 
     i32 test_results = 0, instruction = INSTRUCTION_HALT;
     i32 program[] = {
@@ -34,53 +23,49 @@ int test_instruction_or_0(){
             INSTRUCTION_OR, 255, 256,
     };
 
-    Vm* vm = vm_new();
-
-    vm_init(vm);
-    vm_program_set(vm, program, ARRAY_LENGTH(program));
-
+    VM_INIT(program);
     TEST_BEGIN();
 
     TEST_ASSERT(vm->registers[A] == 0, "vm.registers[A] == 000");
     TEST_ASSERT(vm->registers[B] == 0, "vm.registers[B] == 000");
     TEST_ASSERT(vm->registers[C] == 0, "vm.registers[C] == 000");
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[A] == 255, "vm.registers[A] == 255");
     TEST_ASSERT(vm->registers[B] == 2, "vm.registers[B] == 002");
     TEST_ASSERT(vm->registers[C] == 255, "vm.registers[C] == 255");
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[A] == 255, "vm.registers[A] == 255");
     TEST_ASSERT(vm->registers[B] == 4, "vm.registers[B] == 004");
     TEST_ASSERT(vm->registers[C] == 255, "vm.registers[C] == 255");
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[A] == 255, "vm.registers[A] == 255");
     TEST_ASSERT(vm->registers[B] == 8, "vm.registers[B] == 008");
     TEST_ASSERT(vm->registers[C] == 255, "vm.registers[C] == 255");
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[A] == 255, "vm.registers[A] == 255");
     TEST_ASSERT(vm->registers[B] == 16, "vm.registers[B] == 016");
     TEST_ASSERT(vm->registers[C] == 255, "vm.registers[C] == 016");
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[A] == 255, "vm.registers[A] == 255");
     TEST_ASSERT(vm->registers[B] == 32, "vm.registers[B] == 032");
     TEST_ASSERT(vm->registers[C] == 255, "vm.registers[C] == 255");
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[A] == 255, "vm.registers[A] == 255");
     TEST_ASSERT(vm->registers[B] == 64, "vm.registers[B] == 064");
     TEST_ASSERT(vm->registers[C] == 255, "vm.registers[C] == 255");
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[A] == 255, "vm.registers[A] == 255");
     TEST_ASSERT(vm->registers[B] == 128, "vm.registers[B] == 128");
     TEST_ASSERT(vm->registers[C] == 255, "vm.registers[C] == 128");
 
-    vm_instruction_evaluate(vm, vm_instruction_fetch(vm));
+    TEST_INSTRUCTION_EXECUTE_NEXT(vm);
     TEST_ASSERT(vm->registers[A] == 255, "vm.registers[A] == 255");
     TEST_ASSERT(vm->registers[B] == 256, "vm.registers[B] == 256");
     TEST_ASSERT(vm->registers[C] == 511, "vm.registers[C] == 511");
