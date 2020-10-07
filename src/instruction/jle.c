a19e77f44facd56df320dd40618858a4ca879d56
sirh3e <sirh3e@protonmail.com>
2020-10-07 06:57:20 +0200

    added instruction/jle.c

---
+++ src/instruction/jle.c
@@ -0,0 +1,20 @@
+//
+// Created by sirh3e on 07.10.20.
+//
+
+#include <assert.h>
+
+#include "../instructions.h"
+#include "../registers.h"
+#include "../macros.h"
+
+int instruction_jle(Vm* vm){
+ 
+ assert(vm != NULL);
+ VM_INSTRUCTION_INDEX_ASSERT(vm, 1);
+ 
+ if (vm->registers[C] > vm->registers[D])
+  return 0;
+ 
+ return instruction_jmp(vm);
+}
