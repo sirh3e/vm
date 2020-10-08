6d4c3ef6080e8201ee41ca60f1159bac052362ac
sirh3e <sirh3e@protonmail.com>
2020-10-08 07:42:27 +0200

    imported assert.h

--- src/assert.h
+++ src/assert.h
@@ -5,6 +5,8 @@
 #ifndef VM_ASSERT_H
 #define VM_ASSERT_H
 
+#include <assert.h>
+
 #define VM_ASSERT_STACK_INDEX(vm, amount) assert(vm->stack_index + amount >= 0 && vm->stack_index + amount >= vm->stack_length)
 #define VM_ASSERT_INSTRUCTION_INDEX(vm, amount) assert(vm->instruction_index + amount <= vm->instruction_length)
 #define VM_ASSERT_REGISTER(register_index) assert(register_index < REGISTERS_LENGTH && register_index >= 0)
