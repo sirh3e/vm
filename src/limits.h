c3fb05caade1db6bf06ecb2e61c7aa3f3747af75
sirh3e <sirh3e@protonmail.com>
2020-10-09 06:33:38 +0200

    added files src/limits.h describe all limits for datatypes

---
+++ src/limits.h
@@ -0,0 +1,14 @@
+//
+// Created by sirh3e on 08.10.20.
+//
+
+#ifndef VM_LIMITS_H
+#define VM_LIMITS_H
+
+#define INT32_TYPE_MIN (-2147483648)
+#define INT32_TYPE_MAX 2147483647
+
+#define UINT32_TYPE_MIN 0
+#define UINT32_TYPE_MAX 4294967295
+
+#endif //VM_LIMITS_H
