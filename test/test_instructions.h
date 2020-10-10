aa4943f3df6e615033484b85516a16f2e662c71f
sirh3e <sirh3e@protonmail.com>
2020-10-10 19:20:02 +0200

    added test/instruction/test_jne.c

--- test/test_instructions.h
+++ test/test_instructions.h
@@ -25,6 +25,7 @@ test_vm_instruction_result test_vm_instruction_div();
 test_vm_instruction_result test_vm_instruction_halt();
 test_vm_instruction_result test_vm_instruction_inc();
 test_vm_instruction_result test_vm_instruction_jmp();
+test_vm_instruction_result test_vm_instruction_jne();
 test_vm_instruction_result test_vm_instruction_load();
 test_vm_instruction_result test_vm_instruction_log();
 test_vm_instruction_result test_vm_instruction_mul();
@@ -46,8 +47,9 @@ static test_vm_instruction_result (*tests[])() = {
         test_vm_instruction_div,
         test_vm_instruction_halt,
         test_vm_instruction_inc,
-         */
         test_vm_instruction_jmp,
+         */
+        test_vm_instruction_jne,
         /*
         test_vm_instruction_load,
         test_vm_instruction_log,
