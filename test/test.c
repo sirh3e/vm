10a9f03b1717e1aa288bca3143a4c848b4cccc85
sirh3e <sirh3e@protonmail.com>
2020-10-10 12:51:17 +0200

    refactored test/test.c

--- test/test.c
+++ test/test.c
@@ -11,29 +11,7 @@
 
 int main(){
 
-    i32 test_result = 0;
-
-    u32 (*tests[])() = {
-            test_instruction_add,
-            test_instruction_and,
-            test_instruction_dec,
-            test_instruction_div,
-            test_instruction_halt,
-            test_instruction_inc,
-            test_instruction_load,
-            test_instruction_log,
-            test_instruction_mul,
-            test_instruction_neg,
-            test_instruction_or,
-            test_instruction_pop,
-            test_instruction_push,
-            test_instruction_save,
-            test_instruction_shl,
-            test_instruction_shr,
-            test_instruction_sub,
-            test_instruction_xor,
-    };
-
+    test_result_t test_result = 0;
     for(int i = 0; i < ARRAY_LENGTH(tests); i++){
         test_result |= tests[i]();
     }
