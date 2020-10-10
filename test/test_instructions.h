f4613d5a87ce599980f5f188ce30239143cddda4
sirh3e <sirh3e@protonmail.com>
2020-10-10 12:49:58 +0200

    added all test_instructions into a array

--- test/test_instructions.h
+++ test/test_instructions.h
@@ -5,6 +5,8 @@
 #ifndef VM_TEST_INSTRUCTIONS_H
 #define VM_TEST_INSTRUCTIONS_H
 
+#include "test.h"
+
 test_result_t test_instruction_add();
 test_result_t test_instruction_and();
 test_result_t test_instruction_dec();
@@ -23,5 +25,25 @@ test_result_t test_instruction_shl();
 test_result_t test_instruction_shr();
 test_result_t test_instruction_sub();
 test_result_t test_instruction_xor();
+
+static test_result_t (*tests[])() = {
+        test_instruction_and,
+        test_instruction_dec,
+        test_instruction_div,
+        test_instruction_halt,
+        test_instruction_inc,
+        test_instruction_load,
+        test_instruction_log,
+        test_instruction_mul,
+        test_instruction_neg,
+        test_instruction_or,
+        test_instruction_pop,
+        test_instruction_push,
+        test_instruction_save,
+        test_instruction_shl,
+        test_instruction_shr,
+        test_instruction_sub,
+        test_instruction_xor,
+};
 
 #endif //VM_TEST_INSTRUCTIONS_H
