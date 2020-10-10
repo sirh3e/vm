4f6ff6fb3b145be86ad6dbdbd8be194260d4f4b4
sirh3e <sirh3e@protonmail.com>
2020-10-10 17:45:33 +0200

    added test/instruction/test_jge.c

--- test/instruction/test_jge.c
+++ test/instruction/test_jge.c
@@ -2,3 +2,31 @@
 // Created by sirh3e on 10/10/2020.
 //
 
+#include "../test.h"
+
+test_vm_instruction_result test_vm_instruction_jmp(){
+
+    test_vm_instruction_result test_results = 0;
+    i32 program[] = {
+            INSTRUCTION_LABEL,
+            INSTRUCTION_NOP,
+            INSTRUCTION_JMP, 0
+    };
+
+    VM_INIT(program);
+    TEST_BEGIN();
+
+    for (int i = 0; i < 2; ++i) {
+        TEST_ASSERT(vm->instruction_index == 0, "vm->instruction_index == 0");
+        TEST_ASSERT(vm->instructions[vm->instruction_index] == 2, "vm->instructions[vm->instruction_index] == 2");
+        TEST_INSTRUCTION_EXECUTE_NEXT(vm);
+        TEST_INSTRUCTION_EXECUTE_NEXT(vm);
+        TEST_INSTRUCTION_EXECUTE_NEXT(vm);
+        TEST_ASSERT(vm->instruction_index == 0, "vm->instruction_index == 0");
+        TEST_ASSERT(vm->instructions[vm->instruction_index] == 2, "vm->instructions[vm->instruction_index] == 2");
+    }
+
+    TEST_END(vm);
+
+    return test_results;
+}
--- test/test_instructions.h
+++ test/test_instructions.h
@@ -24,6 +24,7 @@ test_vm_instruction_result test_vm_instruction_dec();
 test_vm_instruction_result test_vm_instruction_div();
 test_vm_instruction_result test_vm_instruction_halt();
 test_vm_instruction_result test_vm_instruction_inc();
+test_vm_instruction_result test_vm_instruction_jge();
 test_vm_instruction_result test_vm_instruction_jmp();
 test_vm_instruction_result test_vm_instruction_load();
 test_vm_instruction_result test_vm_instruction_log();
@@ -47,8 +48,9 @@ static test_vm_instruction_result (*tests[])() = {
         test_vm_instruction_halt,
         test_vm_instruction_inc,
          */
-        test_vm_instruction_jmp,
+        test_vm_instruction_jge,
         /*
+        test_vm_instruction_jmp,
         test_vm_instruction_load,
         test_vm_instruction_log,
         test_vm_instruction_mul,
