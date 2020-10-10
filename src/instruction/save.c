f7893e9339474865db9cb5491ff0cc9e2311bfb4
sirh3e <sirh3e@protonmail.com>
2020-10-10 12:53:37 +0200

    fixed src/instructions/save.c bug fix on assert

--- src/instruction/save.c
+++ src/instruction/save.c
@@ -12,7 +12,7 @@ int instruction_save(Vm* vm) {
 
     assert(vm != NULL);
     VM_INSTRUCTION_INDEX_ASSERT(vm, 1);
-    VM_REGISTER_ASSERT(vm->instructions[vm->instruction_index + 1]);
+    VM_REGISTER_ASSERT(vm->instructions[vm->instruction_index]);
     VM_STACK_INDEX_ASSERT(vm, 1);
 
     vm->stack[vm->stack_index] = vm->registers[vm->instructions[vm->instruction_index]];
