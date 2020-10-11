ee417d1fd23541d4d1355a76d796985002bf0bc5
sirh3e <sirh3e@protonmail.com>
2020-10-11 17:19:49 +0200

    refactored src/assert.h, src/vm.*

--- src/vm.c
+++ src/vm.c
@@ -20,77 +20,79 @@ Vm* vm_new(){
     return vm;
 }
 
-int vm_init(Vm* vm){
+u32 vm_init(Vm* vm){
 
     VM_ASSERT(vm);
 
-    vm->registers = (i32*) malloc(sizeof(i32) * REGISTERS_LENGTH);
+    vm->registers = (i32*) malloc(sizeof(vm->registers) * REGISTERS_LENGTH);
     if(vm->registers == NULL)
-        exit(0); //ToDo find the right error message
+        exit(1); //ToDo find the right error message
 
     vm->stack_allocated = VM_DEFAULT_STACK_ALLOCATED_SIZE;
     vm->stack_index = 0;
-    vm->stack_length = 0;
-    vm->stack = (i32*) malloc(sizeof(i32) * vm->stack_allocated);
+    vm->stack_length = vm->stack_allocated;
+    vm->stack = (i32*) malloc(sizeof(vm->stack) * vm->stack_allocated);
     if(vm->stack == NULL)
-        exit(0); //ToDo find the right error message
+        exit(1); //ToDo find the right error message
 
     vm->instruction_allocated = VM_DEFAULT_INSTRUCTIONS_ALLOCATED_SIZE;
     vm->instruction_index = 0;
-    vm->instruction_length = 0;
-    vm->instructions = (i32*) malloc(sizeof(i32) * vm->instruction_allocated);
+    vm->instruction_length = vm->instruction_allocated;
+    vm->instructions = (i32*) malloc(sizeof(vm->instructions) * vm->instruction_allocated);
     if(vm->instructions == NULL)
-        exit(0); //ToDo find the right error message
+        exit(1); //ToDo find the right error message
 
     return 0;
 }
 
-void vm_free(Vm* vm){ //ToDo
+void vm_free(Vm* vm){
 
-    //free(vm->registers);
-    //free(vm->stack);
-    //free(vm->instructions);
+    free(vm->registers);
+    free(vm->stack);
+    free(vm->instructions);
 
     free(vm);
 }
 
-int vm_program_set(Vm* vm, i32* program, u32 program_length){
+u32 vm_program_set(Vm* vm, i32* program, u32 program_length){
 
     VM_ASSERT(vm);
 
     //ToDo check if the length is larger or smaller then the allocated size
     vm->instruction_length = program_length;
-    memcpy(vm->instructions, program, sizeof(i32) * vm->instruction_length);
+    memcpy(vm->instructions, program, sizeof(vm->instructions) * vm->instruction_length);
 
     return 0;
 }
 
-int vm_program_run(Vm* vm){ //ToDo create a result handler
+u32 vm_program_run(Vm* vm){ //ToDo create a result handler
 
     VM_ASSERT(vm);
 
-    int instruction = INSTRUCTION_HALT, instruction_result = 0;
-
+    int instruction;
     while ((instruction = vm_instruction_fetch(vm)) != INSTRUCTION_HALT){
-        instruction_result = vm_instruction_evaluate(vm, instruction);
+        vm_instruction_result_handler(vm_instruction_evaluate(vm, instruction));
     }
 
     return 0;
 }
 
-int vm_instruction_evaluate(Vm* vm, int instruction){
+u32 vm_instruction_fetch(Vm* vm){
 
     VM_ASSERT(vm);
-    assert(instruction < INSTRUCTION_LENGTH);
-    assert(instruction > 0);
+
+    return vm->instructions[vm->instruction_index++];
+}
+
+vm_instruction_result vm_instruction_evaluate(Vm* vm, int instruction){
+
+    VM_ASSERT(vm);
+    VM_ASSERT_INSTRUCTION(instruction);
 
     return instructions[instruction](vm);
 }
 
-int vm_instruction_fetch(Vm* vm){
+vm_instruction_result vm_instruction_result_handler(vm_instruction_result instruction_result){
 
-    VM_ASSERT(vm);
-    //VM_ASSERT_INSTRUCTION_INDEX(vm, 1);
-
-    return vm->instructions[vm->instruction_index++];
+    return instruction_result;
 }
