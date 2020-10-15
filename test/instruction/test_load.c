50662a49f13f80d09eba2b17b8f228db8d8d62ec
sirh3e <sirh3e@protonmail.com>
2020-10-15 21:30:56 +0200

    refactor test/test_load.c

--- test/instruction/test_load.c
+++ test/instruction/test_load.c
@@ -31,34 +31,34 @@ test_vm_instruction_result test_vm_instruction_load() {
     TEST_BEGIN();
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->stack[vm->stack_index - 1] == 16, "vm->stack[vm->stack_index - 1] == 16");
+    TEST_ASSERT(vm->stack[vm->stack_index - 1] == 16);
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[A] == 16, "vm.registers[A] == 0016");
+    TEST_ASSERT(vm->registers[A] == 16);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->stack[vm->stack_index - 1] == 64, "vm->stack[vm->stack_index - 1] == 64");
+    TEST_ASSERT(vm->stack[vm->stack_index - 1] == 64);
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[A] == 64, "vm.registers[A] == 0064");
+    TEST_ASSERT(vm->registers[A] == 64);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->stack[vm->stack_index - 1] == 128, "vm->stack[vm->stack_index - 1] == 0128");
+    TEST_ASSERT(vm->stack[vm->stack_index - 1] == 128);
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[B] == 128, "vm.registers[B] == 0128");
+    TEST_ASSERT(vm->registers[B] == 128);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->stack[vm->stack_index - 1] == 256, "vm->stack[vm->stack_index - 1] == 0256");
+    TEST_ASSERT(vm->stack[vm->stack_index - 1] == 256);
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[B] == 256, "vm.registers[B] == 0256");
+    TEST_ASSERT(vm->registers[B] == 256);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->stack[vm->stack_index - 1] == 512, "vm->stack[vm->stack_index - 1] == 0512");
+    TEST_ASSERT(vm->stack[vm->stack_index - 1] == 512);
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[C] == 512, "vm.registers[C] == 0512");
+    TEST_ASSERT(vm->registers[C] == 512);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->stack[vm->stack_index - 1] == 1024, "vm->stack[vm->stack_index - 1] == 1024");
+    TEST_ASSERT(vm->stack[vm->stack_index - 1] == 1024);
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[C] == 1024, "vm.registers[C] == 1024");
+    TEST_ASSERT(vm->registers[C] == 1024);
 
     TEST_END(vm);
 
