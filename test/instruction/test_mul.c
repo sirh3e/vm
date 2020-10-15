31198e3cfe215e8c8768fa1cbe53d5d4fb6458b8
sirh3e <sirh3e@protonmail.com>
2020-10-15 21:32:04 +0200

    refactor test/test_mul.c

--- test/instruction/test_mul.c
+++ test/instruction/test_mul.c
@@ -25,49 +25,49 @@ test_vm_instruction_result test_vm_instruction_mul() {
     TEST_BEGIN();
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[A] == -2, "vm.registers[A] == -002");
-    TEST_ASSERT(vm->registers[B] == 2, "vm.registers[B] == 0002");
-    TEST_ASSERT(vm->registers[C] == -4, "vm.registers[C] == -004");
+    TEST_ASSERT(vm->registers[A] == -2);
+    TEST_ASSERT(vm->registers[B] == 2);
+    TEST_ASSERT(vm->registers[C] == -4);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[A] == 2, "vm.registers[A] == 0002");
-    TEST_ASSERT(vm->registers[B] == -2, "vm.registers[B] == -002");
-    TEST_ASSERT(vm->registers[C] == -4, "vm.registers[C] == -004");
+    TEST_ASSERT(vm->registers[A] == 2);
+    TEST_ASSERT(vm->registers[B] == -2);
+    TEST_ASSERT(vm->registers[C] == -4);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[A] == -2, "vm.registers[A] == -002");
-    TEST_ASSERT(vm->registers[B] == -2, "vm.registers[B] == -002");
-    TEST_ASSERT(vm->registers[C] == 4, "vm.registers[C] == 0004");
+    TEST_ASSERT(vm->registers[A] == -2);
+    TEST_ASSERT(vm->registers[B] == -2);
+    TEST_ASSERT(vm->registers[C] == 4);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[A] == 2, "vm.registers[A] == 0002");
-    TEST_ASSERT(vm->registers[B] == 2, "vm.registers[B] == 0002");
-    TEST_ASSERT(vm->registers[C] == 4, "vm.registers[C] == 0004");
+    TEST_ASSERT(vm->registers[A] == 2);
+    TEST_ASSERT(vm->registers[B] == 2);
+    TEST_ASSERT(vm->registers[C] == 4);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[A] == 0, "vm.registers[A] == 0000");
-    TEST_ASSERT(vm->registers[B] == 2, "vm.registers[B] == 0002");
-    TEST_ASSERT(vm->registers[C] == 0, "vm.registers[C] == 0000");
+    TEST_ASSERT(vm->registers[A] == 0);
+    TEST_ASSERT(vm->registers[B] == 2);
+    TEST_ASSERT(vm->registers[C] == 0);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[A] == 2, "vm.registers[A] == 0002");
-    TEST_ASSERT(vm->registers[B] == 0, "vm.registers[B] == 0000");
-    TEST_ASSERT(vm->registers[C] == 0, "vm.registers[C] == 0000");
+    TEST_ASSERT(vm->registers[A] == 2);
+    TEST_ASSERT(vm->registers[B] == 0);
+    TEST_ASSERT(vm->registers[C] == 0);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[A] == 0, "vm.registers[A] == 0000");
-    TEST_ASSERT(vm->registers[B] == 0, "vm.registers[B] == 0000");
-    TEST_ASSERT(vm->registers[C] == 0, "vm.registers[C] == 0000");
+    TEST_ASSERT(vm->registers[A] == 0);
+    TEST_ASSERT(vm->registers[B] == 0);
+    TEST_ASSERT(vm->registers[C] == 0);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[A] == 1, "vm.registers[A] == 0001");
-    TEST_ASSERT(vm->registers[B] == 1, "vm.registers[B] == 0001");
-    TEST_ASSERT(vm->registers[C] == 1, "vm.registers[C] == 0001");
+    TEST_ASSERT(vm->registers[A] == 1);
+    TEST_ASSERT(vm->registers[B] == 1);
+    TEST_ASSERT(vm->registers[C] == 1);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->registers[A] == 1000, "vm.registers[A] == 1000");
-    TEST_ASSERT(vm->registers[B] == 1000, "vm.registers[B] == 1000");
-    TEST_ASSERT(vm->registers[C] == 1000000, "vm.registers[C] == 10000000");
+    TEST_ASSERT(vm->registers[A] == 1000);
+    TEST_ASSERT(vm->registers[B] == 1000);
+    TEST_ASSERT(vm->registers[C] == 1000000);
 
     TEST_END(vm);
 
