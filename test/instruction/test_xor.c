d0bdb5cd771b167681b6037b972e403a66a68f8d
sirh3e <sirh3e@protonmail.com>
2020-10-15 21:38:15 +0200

    refactor test/test_xor.c

--- test/instruction/test_xor.c
+++ test/instruction/test_xor.c
@@ -17,22 +17,22 @@ test_vm_instruction_result test_vm_instruction_xor() {
     TEST_BEGIN();
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->instruction_index == 3, "vm.instruction_index == 3");
-    TEST_ASSERT(vm->registers[A] == 1, "vm.register[A] == 1");
-    TEST_ASSERT(vm->registers[B] == 1, "vm.register[B] == 1");
-    TEST_ASSERT(vm->registers[C] == 0, "vm.register[C] == 0");
+    TEST_ASSERT(vm->instruction_index == 3);
+    TEST_ASSERT(vm->registers[A] == 1);
+    TEST_ASSERT(vm->registers[B] == 1);
+    TEST_ASSERT(vm->registers[C] == 0);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->instruction_index == 6, "vm.instruction_index == 6");
-    TEST_ASSERT(vm->registers[A] == 1, "vm.register[A] == 1");
-    TEST_ASSERT(vm->registers[B] == 2, "vm.register[B] == 2");
-    TEST_ASSERT(vm->registers[C] == 3, "vm.register[C] == 3");
+    TEST_ASSERT(vm->instruction_index == 6);
+    TEST_ASSERT(vm->registers[A] == 1);
+    TEST_ASSERT(vm->registers[B] == 2);
+    TEST_ASSERT(vm->registers[C] == 3);
 
     TEST_INSTRUCTION_EXECUTE_NEXT(vm);
-    TEST_ASSERT(vm->instruction_index == 9, "vm.instruction_index == 9");
-    TEST_ASSERT(vm->registers[A] == 4, "vm.register[A] == 04");
-    TEST_ASSERT(vm->registers[B] == 9, "vm.register[B] == 09");
-    TEST_ASSERT(vm->registers[C] == 13, "vm.register[C] == 13");
+    TEST_ASSERT(vm->instruction_index == 9);
+    TEST_ASSERT(vm->registers[A] == 4);
+    TEST_ASSERT(vm->registers[B] == 9);
+    TEST_ASSERT(vm->registers[C] == 13);
 
     TEST_END(vm);
 
