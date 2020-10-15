56e2a4f641ec78a67327507f46a9a26921fdf5c0
sirh3e <sirh3e@protonmail.com>
2020-10-15 21:30:22 +0200

    refactor test/test_ne.c

--- test/instruction/test_jne.c
+++ test/instruction/test_jne.c
@@ -29,8 +29,8 @@ test_vm_instruction_result test_vm_instruction_jne(){
     TEST_BEGIN();
 
     for (int i = 0; i < 5; ++i) {
-        TEST_ASSERT(vm->instruction_index == 0, "vm->instruction_index == 0");
-        TEST_ASSERT(vm->instructions[vm->instruction_index] == INSTRUCTION_LABEL, "vm->instructions[vm->instruction_index] == INSTRUCTION_LABEL");
+        TEST_ASSERT(vm->instruction_index == 0);
+        TEST_ASSERT(vm->instructions[vm->instruction_index] == INSTRUCTION_LABEL);
         TEST_INSTRUCTION_EXECUTE_NEXT(vm); //label
 
         TEST_INSTRUCTION_EXECUTE_NEXT(vm); //push 2
@@ -40,8 +40,8 @@ test_vm_instruction_result test_vm_instruction_jne(){
 
         TEST_INSTRUCTION_EXECUTE_NEXT(vm); //je C = 2, D = 2 false
 
-        TEST_ASSERT(vm->instruction_index == 11, "vm->instruction_index == 11");
-        TEST_ASSERT(vm->instructions[vm->instruction_index] == INSTRUCTION_NOP, "vm->instructions[vm->instruction_index] == INSTRUCTION_NOP");
+        TEST_ASSERT(vm->instruction_index == 11);
+        TEST_ASSERT(vm->instructions[vm->instruction_index] == INSTRUCTION_NOP);
 
         TEST_INSTRUCTION_EXECUTE_NEXT(vm); //nop
         TEST_INSTRUCTION_EXECUTE_NEXT(vm); //push 1
@@ -51,8 +51,6 @@ test_vm_instruction_result test_vm_instruction_jne(){
     }
 
     TEST_END(vm);
-
-    return test_results;
 
     return test_results;
 }
