d16dc76071719a1a7c0782c6f1ea5ddacaaeda06
sirh3e <sirh3e@protonmail.com>
2020-10-16 12:06:38 +0200

    refactored test/test.c remove unused function

--- test/test.c
+++ test/test.c
@@ -15,16 +15,3 @@ int main(){
 
     return test_result;
 }
-
-int test_assert(int expression, const char* message){
-
-    char* color;
-    char* status;
-
-    color = expression ?  VM_TEST_COLOR_GREEN :  VM_TEST_COLOR_RED;
-    status = expression ? "PASSED" : "FAILED";
-
-    printf("[ %s%s%s ] %s\n", color, status, VM_TEST_COLOR_REST, message);
-
-    return expression == 1 ? 0 : 1;
-}
