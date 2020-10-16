bdb97cf501bf35d6aa231c57a9229e221ab935c4
sirh3e <sirh3e@protonmail.com>
2020-10-16 11:47:03 +0200

    refactor src/util.h

--- src/util.h
+++ src/util.h
@@ -5,7 +5,8 @@
 #ifndef VM_UTIL_H
 #define VM_UTIL_H
 
-#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+#define ARRAY_LENGTH(array) \
+    (sizeof(array) / sizeof(array[0]))
 
 // VM_INSTRUCTION:
 #define VM_INSTRUCTION_GET_BY_INDEX(vm, index) \
