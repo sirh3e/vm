374fe2f736f121a65df5486adc94b1ebd61578df
sirh3e <sirh3e@protonmail.com>
2020-10-17 23:47:53 +0200

    added test case for testing instruction_length

--- test/test.c
+++ test/test.c
@@ -8,11 +8,15 @@
 
 int main() {
 
+    u32 instruction_length = ARRAY_LENGTH(instructions);
     u32 test_length = ARRAY_LENGTH(tests);
+
     test_vm_instruction_result test_results = 0;
     for (u32 i = 0; i < test_length; i++) {
         test_results |= tests[i]();
     }
+
+    TEST_ASSERT_MESSAGE(instruction_length == INSTRUCTION_LENGTH, "checks if all instruction are in instructions");
     TEST_ASSERT_MESSAGE(test_length == INSTRUCTION_LENGTH, "checks if all instructions got a test function");
 
     return test_results;
