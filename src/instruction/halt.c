bd0a6c9e2777a551bf477fbb01b78a1c7f881b2e
sirh3e <sirh3e@protonmail.com>
2020-10-23 22:58:30 +0200

    modifiyed src/instruction/halt.c removed todo

--- src/instruction/halt.c
+++ src/instruction/halt.c
@@ -3,14 +3,16 @@
 //
 
 #include "../assert.h"
+#include "../instructions.h"
 #include "../statuscode.h"
+#include "../util.h"
 #include "../vm.h"
 
 vm_instruction_result vm_instruction_halt(Vm *vm) {
 
     VM_ASSERT(vm);
 
-    //ToDo clear memory enforce to stop
+    VM_INSTRUCTION_SET_BY_INDEX(vm, --vm->instruction_index, INSTRUCTION_HALT);
 
     return STATUS_CODE_SUCCESSFUL;
 }
